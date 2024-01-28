ca2da199508807754f6a01c729f7cc1642c1356c
Harshith <harshith.kotian24@gmail.com>
2024-01-28 23:02:21 +0530

    did add tab?

--- app/src/main/java/com/harshith/news/ui/home/HomeScreens.kt
+++ app/src/main/java/com/harshith/news/ui/home/HomeScreens.kt
@@ -1,6 +1,7 @@
 package com.harshith.news.ui.home
 //Home screen will have a appbar which will be obviously material with all those animations.
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.snapping.rememberSnapFlingBehavior
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -9,15 +10,23 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
@@ -71,6 +80,7 @@ fun HomeFeedScreen(
         is HomeUiState.HasNews -> uiState.newsFeed?.homeFeedNews
         is HomeUiState.NoNews -> emptyList()
     }
+    val tabTitles = listOf("Sports", "Technology", "Entertainment", "Politics", "Others")
     Column {
         if(showTopAppBar){
             NewsTopAppBar(
@@ -81,7 +91,9 @@ fun HomeFeedScreen(
         newsFeed?.let { newsArticles ->
             if (newsArticles.isNotEmpty())
                 TopHeadlines(newsArticle = newsArticles, lazyListState = lazyListState, screenWidth = screenWidth)
-            //NewsTabs()
+            NewsTabs(
+                tabTitles
+            )
         }
     }
 }
@@ -111,14 +123,37 @@ fun TopHeadlines(
 
 @Composable
 fun NewsTabs(
-    onSelected: () -> Unit,
-    onClick: () -> Unit
+    tabTitles: List<String>
 ){
-    Tab(selected = true, onClick = { /*TODO*/ }){
-        Column {
-
+    var state by remember {
+        mutableIntStateOf(0)
+    }
+    tabTitles.forEachIndexed{index, tab ->
+        TabRow(selectedTabIndex = index){
+            Column(
+                Modifier
+                    .padding(10.dp)
+                    .height(50.dp)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Box(
+                    Modifier
+                        .size(10.dp)
+                        .align(Alignment.CenterHorizontally)
+                        .background(
+                            color = if (index == state) MaterialTheme.colorScheme.primary
+                            else MaterialTheme.colorScheme.background
+                        )
+                )
+                Text(
+                    text = tab,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
         }
     }
+
 }
 @Composable
 private fun HomeScreenWithList(
